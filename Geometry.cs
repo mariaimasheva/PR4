5c457d7275bd5ebfaf1bb3b7d8cc4d4e84cd4de7
Borodavko Elena <averush07@gmail.com>
2022-10-14 13:35:08 +0300

    Добавьте файлы проекта.

---
+++ Geometry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        public int RectangleArea(int a, int b) => a * b;
+        public double CylinderVolume(int r, int h) => Math.Round(Math.PI * r * r * h);
+    }
+}
